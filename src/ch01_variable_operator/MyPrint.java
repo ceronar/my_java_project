f4b598f1aab081a432f29ace9b717e90d9568201
wonjinKim <wonjin.kim0911@gmail.com>
2025-07-23 14:29:14 +0900

    두번째 커밋
    add Add.java
    25.07.23 14:26

--- src/ch01_variable_operator/MyPrint.java
+++ src/ch01_variable_operator/MyPrint.java
@@ -1,5 +1,24 @@
 package ch01_variable_operator;
 
+/*
+ * 작성자 : 김원진
+ * 작성일 : 7월 23일
+ * 목적 : 처음 해보는 자바 프로그래맹
+ * */
 public class MyPrint {
-
+    public static void main(String[] args) {
+        // 한줄 짜리 주석(comment)
+        System.out.println("홍"); // 성을 출력합니다
+        System.out.println(); // 엔터를 누릅니다
+        // 이름을 출력합니다
+        System.out.println("길");
+        System.out.println("동");
+        System.out.print("대한 민국");
+        System.out.println(" 화이팅");
+        System.out.println("\"예체능\" 프로그램 메인 MC '강호동'");
+        System.out.println();
+        System.out.println("이름\t국어\t영어\t수학");
+        System.out.println("김철수\t80\t70\t60");
+        System.out.println("박영희\t40\t50\t60");
+    }
 }
