3ee855758eaa3b8a5ca6effb13207f571dfcb9b7
wonjinKim <wonjin.kim0911@gmail.com>
2025-07-23 17:41:58 +0900

    네번째 commit
    25.07.23 17:41

---
+++ src/ch01_variable_operator/MyBox.java
@@ -0,0 +1,17 @@
+package ch01_variable_operator;
+
+public class MyBox {
+    public static void main(String[] args) {
+        int width = 10;
+        int height = 5;
+        int area = width * height;
+        int perimeter = (width + height) * 2;
+        double diagonal = Math.sqrt(width * width + height * height);
+
+        System.out.println("너비 : " + width);
+        System.out.println("높이 : " + height);
+        System.out.println("둘레 : " + perimeter);
+        System.out.println("넓이 : " + area);
+        System.out.println("대각선의 길이 : " + diagonal);
+    }
+}
